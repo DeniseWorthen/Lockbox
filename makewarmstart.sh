c4bfdec7227504e5227848ba6f8259ed114bde38
Denise Worthen <denise.worthen@noaa.gov>
2025-01-09 10:20:48 -0500

    fix makewarmstart

--- makewarmstart.sh
+++ makewarmstart.sh
@@ -12,15 +12,15 @@ datetype1=${year}${mon}${day}.${hour}"0000"
 datetype2=${year}-${mon}-${day}-${hour}
 datetype3=${year}-${mon}-${day}-${secs}
 
-ares=c12
+ares=12
 ores=900
 
 # From control run
-sorc=/work2/noaa/stmp/dworthen/REGRESSION_TEST/cpld_control_${ares}_intel/RESTART
+sorc=/work2/noaa/stmp/dworthen/REGRESSION_TEST/cpld_control_c${ares}_intel/RESTART
 
 # MOM6
 dest=/work2/noaa/stmp/dworthen/input-data-20240501/MOM6_IC/${ores}/2021032306
-cp ${sorc}/RESTART/${datetype1}.MOM.res.nc  ${dest}/MOM.res.nc
+cp ${sorc}/${datetype1}.MOM.res.nc  ${dest}/MOM.res.nc
 #cp ${sorc}/RESTART/MOM.res.${datetype2}-00-00.nc ${dest}/INPUT/MOM.res.nc
 #cp ${sorc}/RESTART/MOM.res.${datetype2}-00-00_1.nc ${dest}/INPUT/MOM.res_1.nc
 #cp ${sorc}/RESTART/MOM.res.${datetype2}-00-00_2.nc ${dest}/INPUT/MOM.res_2.nc
@@ -28,12 +28,12 @@ cp ${sorc}/RESTART/${datetype1}.MOM.res.nc  ${dest}/MOM.res.nc
 
 # CICE6
 dest=/work2/noaa/stmp/dworthen/input-data-20240501/CICE_IC/${ores}/2021032306
-cp ${sorc}/RESTART/iced.${datetype3}.nc ${dest}/INPUT/iced.${datetype3}.nc
+cp ${sorc}/iced.${datetype3}.nc ${dest}/iced.${datetype3}.nc
 ls -1 ${dest}/INPUT/iced.${datetype3}.nc>${dest}/ice.restart_file
 
 # CMEPS
-dest=/work2/noaa/stmp/dworthen/input-data-20240501/CMEPS_IC/${ares}mx${ores}/2021032306
-cp ${sorc}/RESTART/ufs.cpld.cpl.r.${datetype3}.nc ${dest}
+dest=/work2/noaa/stmp/dworthen/input-data-20240501/CMEPS_IC/C${ares}mx${ores}/2021032306
+cp ${sorc}/ufs.cpld.cpl.r.${datetype3}.nc ${dest}
 ls -1 ${dest}/ufs.cpld.cpl.r.${datetype3}.nc>${dest}/rpointer.cpl
 
 
@@ -41,7 +41,7 @@ ls -1 ${dest}/ufs.cpld.cpl.r.${datetype3}.nc>${dest}/rpointer.cpl
 #dest="/scratch1/NCEPDEV/stmp4/Denise.Worthen/input-data_20240501/FV3_input_data48/INPUT_L127_mx500/2021032306"
 
 #FV3 restarts
-dest="/work2/noaa/stmp/dworthen/input-data-20240501/FV3_input_data12/INPUT_L41_mx900/2021032306"
+dest="/work2/noaa/stmp/dworthen/input-data-20240501/FV3_input_data${ares}/INPUT_L41_mx900/2021032306"
 
 #cp ${sorc}/${datetype1}.coupler.res  ${dest}/coupler.res
 cp ${sorc}/${datetype1}.fv_core.res.nc  ${dest}/fv_core.res.nc
