4478fb709a9df32a334199cb78e530b2a895fcb7
denise.worthen <Denise.Worthen@noaa.gov>
2025-04-11 11:15:21 -0500

    update script

--- difflist_v2.sh
+++ difflist_v2.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-cmd=~/cime/tools/cprnc/cprnc
-
 set -x
 
-rt="/scratch1/NCEPDEV/stmp2/Denise.Worthen/FV3_RT"
+rt="/work2/noaa/stmp/dworthen/stmp/dworthen/maskchange/"
 
-src="devcice/datm_cdeps_gfs_intel"
-#src="updcicePR/gfs.b14cedf"
-dst="updcicePR/gfs.01ed4db7c"
+test=cpld_control_gfsv17_intel
 
-#fname="ufs.cpld.cpl.hi.atm.2021-03-22-"
-#time="22320"
+#src=mask.86/${test}
+#dst=mask/${test}
 
-fname="DATM_GFS.cpl.hi.ice.2021-03-22-"
-time="25200"
+src=base/${test}
+dst=mask/${test}
 
-#fname="DATM_CFSR.cpl.hi.ice.2011-10-02-"
-#time="00000"
+fname="ufs.cpld.cpl.hi.ice."
+time="2021-03-22-22320"
 
-$cmd -m ${rt}/${src}/${fname}${time}.nc  ${rt}/${dst}/${fname}${time}.nc
+#fname="RESTART/ufs.cpld.cpl.r."
+#time="2021-03-22-43200"
+
+cprnc -m ${rt}/${src}/${fname}${time}.nc  ${rt}/${dst}/${fname}${time}.nc
+echo "${rt}/${src}/${fname}${time}.nc  ${rt}/${dst}/${fname}${time}.nc"
 
 #for tile in "tile1" "tile2" "tile3" "tile4" "tile5" "tile6"; do
 # $cmd -m ${rt}/base/${fname}${tile}.nc  ${rt}/rest/${fname}${tile}.nc | grep RMS
