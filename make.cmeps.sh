d5e9b4c1c92097d9a292d9d51f943034de0c0564
Denise Worthen <denise.worthen@noaa.gov>
2025-10-22 15:04:45 -0400

    more better cmeps prof

--- make.cmeps.sh
+++ make.cmeps.sh
@@ -2,62 +2,63 @@
 
 set -x
 
-src=tst2
+#src=tst3
 
-#================
-info=rhinit
-
-outfile=${src}/${info}.dat
-if [ -f "$outfile" ]; then
-    rm "$outfile"
-fi
-
-for exp in run0 run1 run2 run3 run4; do
+for src in tst4; do
+#for src in tst1 tst2 tst3 tst4; do
+    #================
+    info=rhinit
     outfile=${src}/${info}.dat
-    #grep 'MED: (med_map_mod: RouteHandles_init)' ${src}/${exp}/ESMF_Profile*| awk '{print $6"  "$8"  "$9"  "$11}'
-    grep 'MED: (med_map_mod: RouteHandles_init)' ${src}/${exp}/ESMF_Profile*| awk '{print $5"  "$7"  "$8"  "$10}'>>${outfile}
-done
+    if [ -f "$outfile" ]; then
+        rm "$outfile"
+    fi
 
-#================
-info=post_ice
-outfile=${src}/${info}.dat
-if [ -f "$outfile" ]; then
-    rm "$outfile"
-fi
+    for exp in run0 run1 run2 run3 run4; do
+        outfile=${src}/${info}.dat
+        grep 'MED: (med_map_mod: RouteHandles_init)' ${src}/${exp}/ESMF_Profile*| awk '{print $5"  "$7"  "$8"  "$10}'>>${outfile}
+    done
 
-for exp in run0 run1 run2 run3 run4; do
-    grep '\[MED] med_phases_post_ice' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
-done
+    #================
+    info=post_ice
+    outfile=${src}/${info}.dat
+    if [ -f "$outfile" ]; then
+        rm "$outfile"
+    fi
 
-#================
-info=post_ocn
-outfile=${src}/${info}.dat
-if [ -f "$outfile" ]; then
-    rm "$outfile"
-fi
+    for exp in run0 run1 run2 run3 run4; do
+        grep '\[MED] med_phases_post_ice' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
+    done
 
-for exp in run0 run1 run2 run3 run4; do
-    grep '\[MED] med_phases_post_ocn' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
-done
+    #================
+    info=post_ocn
+    outfile=${src}/${info}.dat
+    if [ -f "$outfile" ]; then
+        rm "$outfile"
+    fi
 
-#================
-info=post_atm
-outfile=${src}/${info}.dat
-if [ -f "$outfile" ]; then
-    rm "$outfile"
-fi
+    for exp in run0 run1 run2 run3 run4; do
+        grep '\[MED] med_phases_post_ocn' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
+    done
 
-for exp in run0 run1 run2 run3 run4; do
-    grep '\[MED] med_phases_post_atm' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
-done
+    #================
+    info=post_atm
+    outfile=${src}/${info}.dat
+    if [ -f "$outfile" ]; then
+        rm "$outfile"
+    fi
 
-#================
-info=post_wav
-outfile=${src}/${info}.dat
-if [ -f "$outfile" ]; then
-    rm "$outfile"
-fi
+    for exp in run0 run1 run2 run3 run4; do
+        grep '\[MED] med_phases_post_atm' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
+    done
 
-for exp in run0 run1 run2 run3 run4; do
-    grep '\[MED] med_phases_post_wav' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
+    #================
+    info=post_wav
+    outfile=${src}/${info}.dat
+    if [ -f "$outfile" ]; then
+        rm "$outfile"
+    fi
+
+    for exp in run0 run1 run2 run3 run4; do
+        grep '\[MED] med_phases_post_wav' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
+    done
 done
