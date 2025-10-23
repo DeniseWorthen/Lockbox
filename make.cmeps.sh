2f88badf7b5b4bb8a9f9f87afe309ce316bc0da5
Denise Worthen <denise.worthen@noaa.gov>
2025-10-23 06:48:50 -0400

    more better cmeps

--- make.cmeps.sh
+++ make.cmeps.sh
@@ -18,47 +18,16 @@ for src in tst4; do
         grep 'MED: (med_map_mod: RouteHandles_init)' ${src}/${exp}/ESMF_Profile*| awk '{print $5"  "$7"  "$8"  "$10}'>>${outfile}
     done
 
-    #================
-    info=post_ice
-    outfile=${src}/${info}.dat
-    if [ -f "$outfile" ]; then
-        rm "$outfile"
-    fi
+    for info in post_ice post_ocn post_atm post_wav; do
+        #================
+        outfile=${src}/${info}.dat
+        if [ -f "$outfile" ]; then
+            rm "$outfile"
+        fi
 
-    for exp in run0 run1 run2 run3 run4; do
-        grep '\[MED] med_phases_post_ice' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
-    done
-
-    #================
-    info=post_ocn
-    outfile=${src}/${info}.dat
-    if [ -f "$outfile" ]; then
-        rm "$outfile"
-    fi
-
-    for exp in run0 run1 run2 run3 run4; do
-        grep '\[MED] med_phases_post_ocn' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
-    done
-
-    #================
-    info=post_atm
-    outfile=${src}/${info}.dat
-    if [ -f "$outfile" ]; then
-        rm "$outfile"
-    fi
-
-    for exp in run0 run1 run2 run3 run4; do
-        grep '\[MED] med_phases_post_atm' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
-    done
-
-    #================
-    info=post_wav
-    outfile=${src}/${info}.dat
-    if [ -f "$outfile" ]; then
-        rm "$outfile"
-    fi
-
-    for exp in run0 run1 run2 run3 run4; do
-        grep '\[MED] med_phases_post_wav' ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
+        for exp in run0 run1 run2 run3 run4; do
+            grep '\[MED] med_phases_'${info} ${src}/${exp}/ESMF_Profile.summary |awk '{print $4"  "$6"  "$7"  "$9}'>>${outfile}
+        done
     done
 done
+B
