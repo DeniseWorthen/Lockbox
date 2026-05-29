e0f1ccf5593af9f6f051cd89e915f563bd0f7062
Denise Worthen <denise.worthen@noaa.gov>
2026-05-29 06:43:10 -0400

    updates

--- aoflux_refactor/tmp.F90
+++ aoflux_refactor/tmp.F90
@@ -1,27 +1,26 @@
 module flux_atmocn_driver_mod
 
-  use shr_kind_mod,          only : R8=>SHR_KIND_R8, IN=>SHR_KIND_IN ! shared kinds
-  use shr_const_mod,         only : shr_const_spval
-  use shr_sys_mod,           only : shr_sys_abort
-  use shr_strconvert_mod,    only : toString
-  use flux_atmocn_Large_mod, only : flux_atmocn_Large
-  use flux_atmocn_COARE_mod, only : flux_atmocn_COARE
-  use flux_atmocn_UA_mod,    only : flux_atmocn_UA
+  !use shr_kind_mod,          only : R8=>SHR_KIND_R8, IN=>SHR_KIND_IN ! shared kinds
+  !use shr_const_mod,         only : shr_const_spval
+  !use shr_sys_mod,           only : shr_sys_abort
+  !use shr_strconvert_mod,    only : toString
+  use flux_atmocn_bulk_mod, only : flux_atmocn_bulk
+  use flux_atmocn_ccpp_mod, only : flux_atmocn_ccpp
 
   implicit none
   public
 
-  integer, private, parameter :: ocn_flux_scheme_large_and_pond = 0
+  integer, private, parameter :: ocn_flux_scheme_bulk = 0
   integer, private, parameter :: ocn_flux_scheme_ccpp = 1
 
 contains
 
   subroutine flux_atmocn( logunit, nMax, mask,               &
+       ocn_surface_flux_scheme=ocn_surface_flux_scheme,      &
        zbot, ubot, vbot, qbot, rbot, tbot, ts,               &
-       ocn_surface_flux_scheme,                              &
        sen, lat, lwup, taux, tauy, tref, qref, duu10n, evap, &
        us, vs, thbot,                                        &
-       usfc, vsfc, psfc, pbot, lwdn, garea, gcomp, maintask, &
+       gcomp, maintask, usfc, vsfc, psfc, pbot, lwdn, garea, &
        missval,                                              &
        ustar_sv, re_sv, ssq_sv )
 
@@ -36,6 +35,7 @@ contains
     integer,  intent(in) :: logunit
     integer,  intent(in) :: nMax
     real(R8), intent(in) :: mask (nMax)
+    integer,  intent(in) :: ocn_surface_flux_scheme
     real(R8), intent(in) :: zbot (nMax)
     real(R8), intent(in) :: ubot (nMax)
     real(R8), intent(in) :: vbot (nMax)
@@ -43,7 +43,7 @@ contains
     real(R8), intent(in) :: rbot (nMax)
     real(R8), intent(in) :: tbot (nMax)
     real(R8), intent(in) :: ts   (nMax)
-    integer,  intent(in) :: ocn_surface_flux_scheme
+
 
     !-------------------------
     ! required out
@@ -68,14 +68,14 @@ contains
     !-------------------------
     ! optional in (ccpp path)
     !-------------------------
+    type(ESMF_GridComp), intent(in), optional :: gcomp    ! ccpp
+    logical,             intent(in), optional :: maintask ! ccpp
     real(R8), intent(in), optional :: usfc (nMax)         ! ccpp
     real(R8), intent(in), optional :: vsfc (nMax)         ! ccpp
     real(R8), intent(in), optional :: psfc (nMax)         ! ccpp
     real(R8), intent(in), optional :: pbot (nMax)         ! ccpp
     real(R8), intent(in), optional :: lwdn (nMax)         ! ccpp
     real(R8), intent(in), optional :: garea(nMax)         ! ccpp
-    type(ESMF_GridComp), intent(in), optional :: gcomp    ! ccpp
-    logical,             intent(in), optional :: maintask ! ccpp
 
     !-------------------------
     ! optional in/out
@@ -91,10 +91,68 @@ contains
     spval = 0.0_R8
     if (present(missval)) spval = missval
 
-    if (ocn_surface_flux_scheme == ocn_flux_scheme_large_and_pond) then
-       call flux_atmocn_bulk(...)
+    if (ocn_surface_flux_scheme == ocn_flux_scheme_bulk) then
+!       call flux_atmocn_bulk(...)
+  call flux_atmocn_bulk ( logunit=logunit, &
+       nMax=aoflux_in%lsize,               &
+       mask=aoflux_in%mask,                &
+       zbot=aoflux_in%zbot,                &
+       ubot=aoflux_in%ubot,                &
+       vbot=aoflux_in%vbot,                &
+       qbot=aoflux_in%shum,                &
+       rbot=aoflux_in%dens,                &
+       tbot=aoflux_in%tbot,                &
+       ts=aoflux_in%tocn,                  &
+       us=aoflux_in%uocn,                  &
+       vs=aoflux_in%vocn,                  &
+       thbot=aoflux_in%thbot,              &
+       ! optional in
+       missval=0.0_r8,                     &
+       ! out
+       sen=aoflux_out%sen,                 &
+       lat=aoflux_out%lat,                 &
+       lwup=aoflux_out%lwup,               &
+       taux=aoflux_out%taux,               &
+       tauy=aoflux_out%tauy,               &
+       tref=aoflux_out%tref,               &
+       qref=aoflux_out%qref,               &
+       duu10n=aoflux_out%duu10n,           &
+       evap=aoflux_out%evap)
     else if  (ocn_surface_flux_scheme == ocn_flux_scheme_ccpp) then
-       call flux_atmocn_ccpp(...)
+!       call flux_atmocn_ccpp(...)
+  call flux_atmocn_ccpp( logunit=logunit, &
+       nMax=aoflux_in%lsize,              &
+       mask=aoflux_in%mask,               &
+       zbot=aoflux_in%zbot,               &
+       ubot=aoflux_in%ubot,               &
+       vbot=aoflux_in%vbot,               &
+       qbot=aoflux_in%shum,               &
+       rbot=aoflux_in%dens,               &
+       tbot=aoflux_in%tbot,               &
+       ts=aoflux_in%tocn,                 &
+       usfc=aoflux_in%usfc,               &
+       vsfc=aoflux_in%vsfc,               &
+       psfc=aoflux_in%psfc,               &
+       pbot=aoflux_in%pbot,               &
+       lwdn=aoflux_in%lwdn,               &
+       garea=aoflux_in%garea,             &
+       gcomp=gcomp,                       &
+       maintask=maintask,                 &
+       ! optional in
+       missval=0.0_r8,                    &
+       ! out
+       sen=aoflux_out%sen,                &
+       lat=aoflux_out%lat,                &
+       lwup=aoflux_out%lwup,              &
+       taux=aoflux_out%taux,              &
+       tauy=aoflux_out%tauy,              &
+       tref=aoflux_out%tref,              &
+       qref=aoflux_out%qref,              &
+       duu10n=aoflux_out%duu10n,          &
+       evap=aoflux_out%evap,              &
+       ustar_sv=aoflux_out%ustar,         &
+       re_sv=aoflux_out%re,               &
+       ssq_sv=aoflux_out%ssq)
     end if
 
     ! If caller provided optional diagnostics but chosen path does not set them:
