f07c745d61a8b9a33fa9a4e9c6a121498f903005
Denise Worthen <denise.worthen@noaa.gov>
2026-05-29 07:20:18 -0400

    updates

--- aoflux_refactor/tmp.F90
+++ aoflux_refactor/tmp.F90
@@ -15,10 +15,10 @@ module flux_atmocn_driver_mod
 
 contains
 
-  subroutine flux_atmocn( logunit, nMax, mask,               &
-       ocn_surface_flux_scheme=ocn_surface_flux_scheme,      &
+  subroutine flux_atmocn(ocn_surface_flux_scheme=ocn_surface_flux_scheme, &
+       logunit, nMax, mask,                                  &
        zbot, ubot, vbot, qbot, rbot, tbot, ts,               &
-       sen, lat, lwup, taux, tauy, tref, qref, duu10n, evap, &
+       sen, lat, lwup, taux, tauy, evap, tref, qref, duu10n, &
        us, vs, thbot,                                        &
        gcomp, maintask, usfc, vsfc, psfc, pbot, lwdn, garea, &
        missval,                                              &
@@ -32,58 +32,58 @@ contains
     !-------------------------
     ! required in
     !-------------------------
-    integer,  intent(in) :: logunit
-    integer,  intent(in) :: nMax
-    real(R8), intent(in) :: mask (nMax)
-    integer,  intent(in) :: ocn_surface_flux_scheme
-    real(R8), intent(in) :: zbot (nMax)
-    real(R8), intent(in) :: ubot (nMax)
-    real(R8), intent(in) :: vbot (nMax)
-    real(R8), intent(in) :: qbot (nMax)
-    real(R8), intent(in) :: rbot (nMax)
-    real(R8), intent(in) :: tbot (nMax)
-    real(R8), intent(in) :: ts   (nMax)
+    integer,  intent(in) :: ocn_surface_flux_scheme  ! flux scheme
+    integer,  intent(in) :: logunit     !
+    integer,  intent(in) :: nMax        ! data vector length
+    real(R8), intent(in) :: mask (nMax) ! ocn domain mask 0 <=> out of domain
 
+    real(R8), intent(in) :: zbot (nMax) ! atm level height (m)
+    real(R8), intent(in) :: ubot (nMax) ! atm u wind (bottom) (m/s)
+    real(R8), intent(in) :: vbot (nMax) ! atm v wind (bottom) (m/s)
+    real(R8), intent(in) :: qbot (nMax) ! atm specific humidity (bottom) (kg/kg)
+    real(R8), intent(in) :: rbot (nMax) ! atm air density (kg/m^3)
+    real(R8), intent(in) :: tbot (nMax) ! atm T (bottom) (K)
+    real(R8), intent(in) :: ts   (nMax) ! ocn temperature (K)
 
     !-------------------------
     ! required out
     !-------------------------
-    real(R8), intent(out) :: sen   (nMax)
-    real(R8), intent(out) :: lat   (nMax)
-    real(R8), intent(out) :: lwup  (nMax)
-    real(R8), intent(out) :: taux  (nMax)
-    real(R8), intent(out) :: tauy  (nMax)
-    real(R8), intent(out) :: tref  (nMax)
-    real(R8), intent(out) :: qref  (nMax)
-    real(R8), intent(out) :: duu10n(nMax)
-    real(R8), intent(out) :: evap  (nMax)
+    real(R8), intent(out) :: sen   (nMax) ! heat flux: sensible (W/m^2)
+    real(R8), intent(out) :: lat   (nMax) ! heat flux: latent (W/m^2)
+    real(R8), intent(out) :: lwup  (nMax) ! heat flux: lw upward (W/m^2)
+    real(R8), intent(out) :: taux  (nMax) ! surface stress, zonal (N)
+    real(R8), intent(out) :: tauy  (nMax) ! surface stress, maridional (N)
+    real(R8), intent(out) :: evap  (nMax) ! heat flux ((kg/s)/m^2)
+    real(R8), intent(out) :: tref  (nMax) ! diagnostic : 2m ref height T (K)
+    real(R8), intent(out) :: qref  (nMax) ! diagnostic:  2m ref humidity (kg/kg)
+    real(R8), intent(out) :: duu10n(nMax) ! diagnostic: 10m wind speed squared (m/s)^2
 
     !-------------------------
     ! optional in (bulk path)
     !-------------------------
-    real(R8), intent(in), optional :: us   (nMax)   ! bulk
-    real(R8), intent(in), optional :: vs   (nMax)   ! bulk
-    real(R8), intent(in), optional :: thbot(nMax)   ! bulk
+    real(R8), intent(in), optional :: us   (nMax) ! ocn u-velocity (m/s)
+    real(R8), intent(in), optional :: vs   (nMax) ! ocn v-velocity (m/s)
+    real(R8), intent(in), optional :: thbot(nMax) ! atm potential T (K)
 
     !-------------------------
     ! optional in (ccpp path)
     !-------------------------
-    type(ESMF_GridComp), intent(in), optional :: gcomp    ! ccpp
-    logical,             intent(in), optional :: maintask ! ccpp
-    real(R8), intent(in), optional :: usfc (nMax)         ! ccpp
-    real(R8), intent(in), optional :: vsfc (nMax)         ! ccpp
-    real(R8), intent(in), optional :: psfc (nMax)         ! ccpp
-    real(R8), intent(in), optional :: pbot (nMax)         ! ccpp
-    real(R8), intent(in), optional :: lwdn (nMax)         ! ccpp
-    real(R8), intent(in), optional :: garea(nMax)         ! ccpp
+    type(ESMF_GridComp), intent(in), optional :: gcomp    ! gridded component
+    logical,             intent(in), optional :: maintask ! main task
+    real(R8), intent(in), optional :: garea(nMax) ! grid area (m^2)
+    real(R8), intent(in), optional :: usfc (nMax) ! atm u wind (surface) (m/s)
+    real(R8), intent(in), optional :: vsfc (nMax) ! atm v wind (surface) (m/s)
+    real(R8), intent(in), optional :: psfc (nMax) ! atm P (surface) (Pa)
+    real(R8), intent(in), optional :: pbot (nMax) ! atm P (bottom) (Pa)
+    real(R8), intent(in), optional :: lwdn (nMax) ! atm lw downward (W/m^2)
 
     !-------------------------
     ! optional in/out
     !-------------------------
-    real(R8), intent(in),  optional :: missval
-    real(R8), intent(out), optional :: ustar_sv(nMax)
-    real(R8), intent(out), optional :: re_sv   (nMax)
-    real(R8), intent(out), optional :: ssq_sv  (nMax)
+    real(R8), intent(in),  optional :: missval         ! masked value
+    real(R8), intent(out), optional :: ustar_sv(nMax)  ! diagnostic: ustar
+    real(R8), intent(out), optional :: re_sv   (nMax)  ! diagnostic: sqrt of exchange coeff (water)
+    real(R8), intent(out), optional :: ssq_sv  (nMax)  ! diagnostic: sea surface humidity (kg/kg)
 
     ! local
     real(R8) :: spval
@@ -114,10 +114,11 @@ contains
        lwup=aoflux_out%lwup,               &
        taux=aoflux_out%taux,               &
        tauy=aoflux_out%tauy,               &
+       evap=aoflux_out%evap,               &
        tref=aoflux_out%tref,               &
        qref=aoflux_out%qref,               &
-       duu10n=aoflux_out%duu10n,           &
-       evap=aoflux_out%evap)
+       duu10n=aoflux_out%duu10n)
+
     else if  (ocn_surface_flux_scheme == ocn_flux_scheme_ccpp) then
 !       call flux_atmocn_ccpp(...)
   call flux_atmocn_ccpp( logunit=logunit, &
@@ -130,14 +131,14 @@ contains
        rbot=aoflux_in%dens,               &
        tbot=aoflux_in%tbot,               &
        ts=aoflux_in%tocn,                 &
+       gcomp=gcomp,                       &
+       maintask=maintask,                 &
+       garea=aoflux_in%garea,             &
        usfc=aoflux_in%usfc,               &
        vsfc=aoflux_in%vsfc,               &
        psfc=aoflux_in%psfc,               &
        pbot=aoflux_in%pbot,               &
        lwdn=aoflux_in%lwdn,               &
-       garea=aoflux_in%garea,             &
-       gcomp=gcomp,                       &
-       maintask=maintask,                 &
        ! optional in
        missval=0.0_r8,                    &
        ! out
@@ -146,10 +147,10 @@ contains
        lwup=aoflux_out%lwup,              &
        taux=aoflux_out%taux,              &
        tauy=aoflux_out%tauy,              &
+       evap=aoflux_out%evap,              &
        tref=aoflux_out%tref,              &
        qref=aoflux_out%qref,              &
        duu10n=aoflux_out%duu10n,          &
-       evap=aoflux_out%evap,              &
        ustar_sv=aoflux_out%ustar,         &
        re_sv=aoflux_out%re,               &
        ssq_sv=aoflux_out%ssq)
