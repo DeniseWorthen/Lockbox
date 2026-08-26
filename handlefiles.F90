2eb33fabaffc01b14795539cb4100e2827929f49
Denise Worthen <denise.worthen@noaa.gov>
2026-08-26 06:35:46 -0400

    driver 2, handlefiles

---
+++ handlefiles.F90
@@ -0,0 +1,49 @@
+  !call handlefiles(isroot, state_n%filename, 'complete', l_use_filesize, rc=rc)
+subroutine handlefiles(isroot, fname, use_filesize, mode, rc=rc)
+
+  logical,            intent(in)  :: isroot
+  character, len=(*), intent(in)  :: fname
+  logical,            intent(in)  :: use_filesize
+  character(len=*),   intent(in)  :: mode
+  integer,            intent(out) :: rc
+
+  rc = 0
+
+  select case (mode)
+
+  case ('create')
+     if (isroot) then
+        call create_schema(fname)
+        if (use_filesize) then
+           call write_record(fname)
+        else
+           call write_padding(fname)
+        endif
+     endif
+
+  case ('complete')
+     if (isroot) then
+        if (use_filesize) then
+           call write_bulk_data(fname)   ! fsize grows past createsize
+        else
+           call write_record(fname)      ! nlen 0->1
+        endif
+     endif
+
+  case('create-complete')
+     if (isroot) then
+        call create_schema(fname)
+        if (use_filesize) then
+           call write_record(fname)
+           call write_bulk_data(fname)   ! fsize grows past createsize
+        else
+           call write_padding(fname)
+           call write_record(fname)      ! nlen 0->1
+        endif
+     endif
+
+  case default
+     ierr = 1
+  end select
+
+end subroutine handlefiles
